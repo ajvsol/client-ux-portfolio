762d914166736a6ff976907a4c88409f9d99baa6
Aaron <22418751+ajvsol@users.noreply.github.com>
2023-03-31 20:15:28 +0100

    style: grid columns setup

---
+++ pages/synergy-cafe/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function SynergyCafe() {
+  return (
+    <div>
+      <h1>Synergy Cafe</h1>
+      <h2>Project overview</h2>
+      <div>
+        <h2>The problem</h2>
+        <p>Lorem ipsum</p>
+      </div>
+      <div>
+        <h2>The goal</h2>
+        <p>Lorem ipsum</p>
+      </div>
+      <div>
+        <h2>My role in the project</h2>
+        <p>Lorem ipsum</p>
+      </div>
+    </div>
+  );
+}
